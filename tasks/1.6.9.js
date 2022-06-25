10a5525935dea3e2b6419956c15aaae426d193d3
NemoRock <lexa.brus97@gmail.com>
2022-06-25 16:25:01 +0500

    added task 1.6.9

---
+++ tasks/1.6.9.js
@@ -0,0 +1,89 @@
+function work(a, b) {
+    alert( a + b )
+}
+function spy(func){
+    function wrapper(...args){
+        wrapper.calls.push(args)
+        return func.apply(this, arguments)
+    }
+    wrapper.calls = []
+    return wrapper
+}
+work = spy(work)
+work(1, 2) // 3
+work(4, 5) // 9
+
+for (let args of work.calls) {
+    alert( 'call:' + args.join() ) // "call:1,2", "call:4,5"
+}
+
+
+
+function f(x) {
+    alert(x)
+}
+function delay(f, ms){
+    return function (...args){
+        let savedThis=this
+        setTimeout(function (){
+            f.apply(savedThis,args)
+        },ms)
+    }
+}
+let f1000 = delay(f, 1000)
+let f1500 = delay(f, 4500)
+f1000("test") // показывает "test" после 1000 мс
+f1500("test") // показывает "test" после 4500 мс
+
+
+function debounce(f, ms){
+    let isCooldown = false
+    return function (){
+        if (isCooldown)return
+        f.apply(this, arguments)
+        isCooldown = true
+        setTimeout(()=>isCooldown=false, ms)
+    }
+}
+let f = debounce(alert, 1000)
+f(1) // выполняется немедленно
+f(2) // проигнорирован
+setTimeout( () => f(3), 100) // проигнорирован (прошло только 100 мс)
+setTimeout( () => f(4), 1100) // выполняется
+setTimeout( () => f(5), 1500) // проигнорирован (прошло только 400 мс от последнего вызова)
+
+
+
+
+function throttle(func, ms) {
+    let isThrottled = false,
+        savedArgs,
+        savedThis
+    function wrapper() {
+        if (isThrottled) { // (2)
+            savedArgs = arguments
+            savedThis = this;
+            return
+        }
+        func.apply(this, arguments) // (1)
+        isThrottled = true
+        setTimeout(function() {
+            isThrottled = false // (3)
+            if (savedArgs) {
+                wrapper.apply(savedThis, savedArgs)
+                savedArgs = savedThis = null
+            }
+        }, ms)
+    }
+    return wrapper
+}
+function f(a) {
+    console.log(a)
+}
+// f1000 передаёт вызовы f максимум раз в 1000 мс
+let f1000 = throttle(f, 1000)
+f1000(1) // показывает 1
+f1000(2) // (ограничение, 1000 мс ещё нет)
+f1000(3) // (ограничение, 1000 мс ещё нет)
+// когда 1000 мс истекли ...
+// ...выводим 3, промежуточное значение 2 было проигнорировано
